58a9e381b2da6c74f47e092176f6eafb541a642d
redhoodsu <redhoodsu@tencent.com>
2019-09-01 13:55:45 +0800

    release: v1.1.1

--- eruda-fps.js
+++ eruda-fps.js
@@ -1,200 +1,215 @@
 // Modified from https://github.com/mrdoob/stats.js/
 
-(function(root, factory)
-{
-    if (typeof define === 'function' && define.amd)
-    {
-        define([], factory);
-    } else if (typeof module === 'object' && module.exports)
-    {
-        module.exports = factory();
-    } else { root.erudaFps = factory(); }
-}(this, function ()
-{
-    var w = window.innerWidth;
+;(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory)
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory()
+  } else {
+    root.erudaFps = factory()
+  }
+})(this, function() {
+  var w = window.innerWidth
 
-    return function (eruda)
-    {
-        var WIDTH = w,
-            HEIGHT = 192,
-            TEXT_X = 12,
-            TEXT_Y = 8,
-            GRAPH_X = 12,
-            GRAPH_Y = 40,
-            GRAPH_WIDTH = w - 24,
-            GRAPH_HEIGHT = 142,
-            BACK_COLOR = '#fff',
-            FORE_COLOR = '#90caf9',
-            STEP = 2,
-            NAME = 'FPS';
+  return function(eruda) {
+    var WIDTH = w,
+      HEIGHT = 192,
+      TEXT_X = 12,
+      TEXT_Y = 8,
+      GRAPH_X = 12,
+      GRAPH_Y = 40,
+      GRAPH_WIDTH = w - 24,
+      GRAPH_HEIGHT = 142,
+      BACK_COLOR = '#fff',
+      FORE_COLOR = '#90caf9',
+      STEP = 2,
+      NAME = 'FPS'
 
-        var util = eruda.util,
-            Settings = eruda.Settings,
-            round = Math.round,
-            config = eruda.config,
-            Tool = eruda.Tool;
+    var util = eruda.util,
+      Settings = eruda.Settings,
+      round = Math.round,
+      Tool = eruda.Tool
 
-        var Fps = Tool.extend({
-            name: 'fps',
-            init: function ($el, container)
-            {
-                this.callSuper(Tool, 'init', arguments);
+    var Fps = Tool.extend({
+      name: 'fps',
+      init: function($el, container) {
+        this.callSuper(Tool, 'init', arguments)
 
-                this._style = util.evalCss([
-                    '.eruda-fps {padding: 10px !important;}',
-                    'canvas {width: 100%; border-radius: 4px; box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.05), 0 1px 4px 0 rgba(0, 0, 0, 0.08), 0 3px 1px -2px rgba(0, 0, 0, 0.2);}'
-                ].join('.eruda-fps '));
-                this._isRunning = false;
-                this._beginTime = util.now();
-                this._prevTime = this._beginTime;
-                this._frames = 0;
-                this._min = Infinity;
-                this._max = 0;
-                this._alwaysActivated = true;
-                this._appendTpl();
-                this._initCanvas();
-                this._initCfg(container);
-            },
-            show: function ()
-            {
-                this._start();
+        this._style = util.evalCss(
+          [
+            '.eruda-fps {padding: 10px !important;}',
+            'canvas {width: 100%; border-radius: 4px; box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.05), 0 1px 4px 0 rgba(0, 0, 0, 0.08), 0 3px 1px -2px rgba(0, 0, 0, 0.2);}'
+          ].join('.eruda-fps ')
+        )
+        this._container = container
+        this._isRunning = false
+        this._beginTime = util.now()
+        this._prevTime = this._beginTime
+        this._frames = 0
+        this._min = Infinity
+        this._max = 0
+        this._alwaysActivated = true
+        this._appendTpl()
+        this._initCanvas()
+        this._initCfg()
+      },
+      show: function() {
+        this._start()
 
-                this.callSuper(Tool, 'show', arguments);
-            },
-            hide: function ()
-            {
-                if (!this._alwaysActivated) this._stop();
+        this.callSuper(Tool, 'show', arguments)
+      },
+      hide: function() {
+        if (!this._alwaysActivated) this._stop()
 
-                this.callSuper(Tool, 'hide', arguments);
-            },
-            destroy: function () 
-            {
-                this._stop();
-                util.evalCss.remove(this._style);
-                this.callSuper(Tool, 'destroy', arguments);
-            },
-            _start: function ()
-            {
-                if (this._isRunning) return;
+        this.callSuper(Tool, 'hide', arguments)
+      },
+      destroy: function() {
+        this._stop()
+        util.evalCss.remove(this._style)
+        this.callSuper(Tool, 'destroy', arguments)
+        this._rmCfg()
+      },
+      _rmCfg: function() {
+        var cfg = this.config
 
-                var self = this;
+        var settings = this._container.get('settings')
+        if (!settings) return
 
-                this._isRunning = true;
+        settings.remove(cfg, 'alwaysActivated').remove('Fps')
+      },
+      _start: function() {
+        if (this._isRunning) return
 
-                function loop()
-                {
-                    if (!self._isRunning) return;
+        var self = this
 
-                    self._update();
-                    requestAnimationFrame(loop);
-                }
+        this._isRunning = true
 
-                loop();
-            },
-            _stop: function ()
-            {
-                this._isRunning = false;
-                this._beginTime = util.now();
-                this._prevTime = this._beginTime;
-                this._frames = 0;
-            },
-            _appendTpl: function ()
-            {
-                this._$el.html('<canvas></canvas>');
+        function loop() {
+          if (!self._isRunning) return
 
-                this._canvas = this._$el.find('canvas').get(0);
-                this._ctx = this._canvas.getContext('2d');
-            },
-            _initCanvas: function ()
-            {
-                var canvas = this._canvas,
-                    ctx = this._ctx;
+          self._update()
+          requestAnimationFrame(loop)
+        }
 
-                canvas.width = WIDTH;
-                canvas.height = HEIGHT;
+        loop()
+      },
+      _stop: function() {
+        this._isRunning = false
+        this._beginTime = util.now()
+        this._prevTime = this._beginTime
+        this._frames = 0
+      },
+      _appendTpl: function() {
+        this._$el.html('<canvas></canvas>')
 
-                ctx.font = 'bold 18px Helvetica,Arial,sans-serif';
-                ctx.textBaseline = 'top';
+        this._canvas = this._$el.find('canvas').get(0)
+        this._ctx = this._canvas.getContext('2d')
+      },
+      _initCanvas: function() {
+        var canvas = this._canvas,
+          ctx = this._ctx
 
-                ctx.fillStyle = BACK_COLOR;
-                ctx.fillRect(0, 0, WIDTH, HEIGHT);
+        canvas.width = WIDTH
+        canvas.height = HEIGHT
 
-                ctx.fillStyle = FORE_COLOR;
-                ctx.fillText(NAME, TEXT_X, TEXT_Y);
-                ctx.fillRect(GRAPH_X, GRAPH_Y, GRAPH_WIDTH, GRAPH_HEIGHT);
+        ctx.font = 'bold 18px Helvetica,Arial,sans-serif'
+        ctx.textBaseline = 'top'
 
-                ctx.fillStyle = BACK_COLOR;
-                ctx.globalAlpha = 0.9;
-                ctx.fillRect(GRAPH_X, GRAPH_Y, GRAPH_WIDTH, GRAPH_HEIGHT);
-            },
-            _initCfg: function (container)
-            {
-                var cfg = this.config = Settings.createCfg('eruda-fps', {
-                    alwaysActivated: true
-                });
+        ctx.fillStyle = BACK_COLOR
+        ctx.fillRect(0, 0, WIDTH, HEIGHT)
 
-                if (!cfg.get('alwaysActivated')) this._alwaysActivated = false;
+        ctx.fillStyle = FORE_COLOR
+        ctx.fillText(NAME, TEXT_X, TEXT_Y)
+        ctx.fillRect(GRAPH_X, GRAPH_Y, GRAPH_WIDTH, GRAPH_HEIGHT)
 
-                var self = this;
+        ctx.fillStyle = BACK_COLOR
+        ctx.globalAlpha = 0.9
+        ctx.fillRect(GRAPH_X, GRAPH_Y, GRAPH_WIDTH, GRAPH_HEIGHT)
+      },
+      _initCfg: function(container) {
+        var container = this._container
+        var cfg = (this.config = Settings.createCfg('fps', {
+          alwaysActivated: true
+        }))
 
-                cfg.on('change', function (key, val)
-                {
-                    switch (key)
-                    {
-                        case 'alwaysActivated': self._alwaysActivated = val; return;
-                    }
-                });
+        if (!cfg.get('alwaysActivated')) this._alwaysActivated = false
 
-                var settings = container.get('settings');
+        var self = this
 
-                settings.text('Fps')
-                    .switch(cfg, 'alwaysActivated', 'Always Activated')
-                    .separator();
-            },
-            _update: function ()
-            {
-                this._frames++;
+        cfg.on('change', function(key, val) {
+          switch (key) {
+            case 'alwaysActivated':
+              self._alwaysActivated = val
+              return
+          }
+        })
 
-                var prevTime = this._prevTime,
-                    time = util.now();
+        var settings = container.get('settings')
 
-                // Only update one time per second.
-                if (time > prevTime + 1000)
-                {
-                    this._draw((this._frames * 1000) / (time - prevTime), 100);
-                    this._prevTime = time;
-                    this._frames = 0;
-                }
+        settings
+          .text('Fps')
+          .switch(cfg, 'alwaysActivated', 'Always Activated')
+          .separator()
+      },
+      _update: function() {
+        this._frames++
 
-                this._beginTime = time;
-            },
-            _draw: function (val, maxVal)
-            {
-                this._min = Math.min(this._min, val);
-                this._max = Math.max(this._max, val);
+        var prevTime = this._prevTime,
+          time = util.now()
 
-                var min = this._min,
-                    max = this._max,
-                    canvas = this._canvas,
-                    ctx = this._ctx;
+        // Only update one time per second.
+        if (time > prevTime + 1000) {
+          this._draw((this._frames * 1000) / (time - prevTime), 100)
+          this._prevTime = time
+          this._frames = 0
+        }
 
-                ctx.fillStyle = BACK_COLOR;
-                ctx.globalAlpha = 1;
-                ctx.fillRect(0, 0, WIDTH, GRAPH_Y);
-                ctx.fillStyle = FORE_COLOR;
-                ctx.fillText(round(val) + '' + NAME + ' (' + round(min) + '-' + round(max) + ')',
-                    TEXT_X, TEXT_Y);
+        this._beginTime = time
+      },
+      _draw: function(val, maxVal) {
+        this._min = Math.min(this._min, val)
+        this._max = Math.max(this._max, val)
 
-                ctx.drawImage(canvas, GRAPH_X + STEP, GRAPH_Y, GRAPH_WIDTH - STEP, GRAPH_HEIGHT, GRAPH_X, GRAPH_Y, GRAPH_WIDTH - STEP, GRAPH_HEIGHT);
+        var min = this._min,
+          max = this._max,
+          canvas = this._canvas,
+          ctx = this._ctx
 
-                ctx.fillRect(GRAPH_X + GRAPH_WIDTH - STEP, GRAPH_Y, STEP, GRAPH_HEIGHT);
+        ctx.fillStyle = BACK_COLOR
+        ctx.globalAlpha = 1
+        ctx.fillRect(0, 0, WIDTH, GRAPH_Y)
+        ctx.fillStyle = FORE_COLOR
+        ctx.fillText(
+          round(val) + '' + NAME + ' (' + round(min) + '-' + round(max) + ')',
+          TEXT_X,
+          TEXT_Y
+        )
 
-                ctx.fillStyle = BACK_COLOR;
-                ctx.globalAlpha = 0.9;
-                ctx.fillRect(GRAPH_X + GRAPH_WIDTH - STEP, GRAPH_Y, STEP, round((1 - (val / maxVal)) * GRAPH_HEIGHT));
-            }
-        });
+        ctx.drawImage(
+          canvas,
+          GRAPH_X + STEP,
+          GRAPH_Y,
+          GRAPH_WIDTH - STEP,
+          GRAPH_HEIGHT,
+          GRAPH_X,
+          GRAPH_Y,
+          GRAPH_WIDTH - STEP,
+          GRAPH_HEIGHT
+        )
 
-        return new Fps();
-    };
-}));
+        ctx.fillRect(GRAPH_X + GRAPH_WIDTH - STEP, GRAPH_Y, STEP, GRAPH_HEIGHT)
+
+        ctx.fillStyle = BACK_COLOR
+        ctx.globalAlpha = 0.9
+        ctx.fillRect(
+          GRAPH_X + GRAPH_WIDTH - STEP,
+          GRAPH_Y,
+          STEP,
+          round((1 - val / maxVal) * GRAPH_HEIGHT)
+        )
+      }
+    })
+
+    return new Fps()
+  }
+})
